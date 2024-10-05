9540332295bbf04c2db9b7b315e606cd0232ad1d
Liam York <yorkliam08@gmail.com>
2024-10-05 18:50:54 -0500

    Turn based Battles

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -1,31 +1,138 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
 public class BattleSystem : MonoBehaviour
 {
 
-    public GameObject playerPrefab;
+    public GameObject littleGuyPrefab;
     public GameObject enemyPrefab;
 
     public Transform playerBattleStation;
     public Transform enemyBattleStation;
 
+    LittleGuyzStats playerLittleGuy;
+
+    LittleGuyzStats enemyLittleGuy;
+
+    public TextMeshProUGUI dialogueText;
+
+    public BattleHud playerHUD;
+
+    public BattleHud enemyHud;
+
     public BattleState state;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
         state = BattleState.START;
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    void SetupBattle()
+    IEnumerator SetupBattle()
     {
-        Instantiate(playerPrefab, playerBattleStation);
-        Instantiate(enemyPrefab, enemyBattleStation);
+        GameObject playerGo = Instantiate(littleGuyPrefab, playerBattleStation);
+        playerLittleGuy = playerGo.GetComponent<LittleGuyzStats>();
+
+        GameObject enemyGo = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyLittleGuy = enemyGo.GetComponent<LittleGuyzStats>();
+
+        dialogueText.text = " A wild Little Guy" + enemyLittleGuy.unitName + "Gets you";
+
+        playerHUD.SetHUD( playerLittleGuy);
+        enemyHud.SetHUD(enemyLittleGuy);
+
+        yield return new WaitForSeconds(2f);
+
+        if(playerLittleGuy.swiftness < enemyLittleGuy.swiftness)
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+        if( enemyLittleGuy.swiftness<playerLittleGuy.swiftness)
+        {
+            state = BattleState.ENEMYTURN;
+            EnemyTurn();
+        }
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyLittleGuy.TakeDamage(playerLittleGuy.violence);
+        dialogueText.text = "The attack hits";
+
+        yield return new WaitForSeconds(2f);
+
+        if(isDead)
+        {
+            state = BattleState.WON;
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+         dialogueText.text = enemyLittleGuy.unitName + "Attacks";
+
+         yield return new WaitForSeconds(1f);
+
+         bool isDead = playerLittleGuy.TakeDamage(enemyLittleGuy.violence);
+
+         playerHUD.SetHP(playerLittleGuy.currentHealth);
+
+         yield return new WaitForSeconds(1f);
+
+         if(isDead)
+         {
+            state=BattleState.LOST;
+            
+            EndBattle();
+         }
+         else
+         {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+         }
+
+
+        
+    }
+
+    void EndBattle()
+    {
+        if( state == BattleState.WON)
+        {
+            dialogueText.text = "You win";
+            playerLittleGuy.exp = playerLittleGuy.exp + 10;
+        }
+        else if(state == BattleState.LOST)
+        {
+             dialogueText.text = "DIE";
+        }
+    }
+    void PlayerTurn()
+    {
+        dialogueText.text = "Make your move";
+    }
+
+    public void OnAttackButton()
+    {
+        if(state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+
+        StartCoroutine(PlayerAttack());
     }
 
 
